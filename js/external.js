2746ca9eaff6416af8c1d263023be3ff464da9ca
yuriprotasio <yuriprotasio@hotmail.com>
2022-12-16 00:12:28 -0300

    fix url

--- js/external.js
+++ js/external.js
@@ -2,13 +2,15 @@ function checkExternal () {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const token = urlParams.get('token')
+  const expires = urlParams.get('Expires')
+  const signature = urlParams.get('Signature')
   if (!token) {
     window.location.href = 'index.html'
   }
   var video = document.getElementById('videoPlayer')
   var source = document.createElement('source');
   
-  source.setAttribute('src', token)
+  source.setAttribute('src', token + `&Expires=${expires}&Signature=${signature}`)
   source.setAttribute('type', 'video/mp4');
   video.appendChild(source)
 }
